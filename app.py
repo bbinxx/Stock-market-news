290de8b967e9c92f8032c9dd8c5bd79ea6b1e52e
Bibin <bibinraju541@gmail.com>
2024-06-08 17:14:18 +0530

    search and display working

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, flash
-from dotenv import load_dotenv
+from dotenv import load_dotenv # type: ignore
 import requests
+
 load_dotenv() 
 
-url = "https://newsapi.org/v2/everything?q=AAPL&apiKey=ace4a856372147739d32a3a7cf3f004e"
-
-response = requests.get(url)
-data = response.json()
+api_key = os.getenv("apiKey")
 
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
-    return render_template("index.html")
-    # return data
+    # Get search query from request object
+    search_query = request.args.get("search_query")  # Access using 'args' for GET requests
 
+    # If search query exists, update API request URL
+    if search_query:
+        url = f"https://newsapi.org/v2/everything?q={search_query}&apiKey={api_key}"
+        response = requests.get(url)
+        data = response.json()
+        # print(data)
+    else:
+        # Handle case where no search query is provided
+        data = None  # Or set a default message
+
+    return render_template("index.html", news_data=data, search_query=search_query)
 
 
 
